d20920c7e6201bca8b7d43a0ebcf4c923e56aa73
unknown <zalavishmayraj@gmail.com>
2026-01-18 17:35:30 +0530

    GDACS ingestion pipeline complete

--- retrieve.py
+++ retrieve.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import pandas as pd
 
 url = "https://www.gdacs.org/gdacsapi/api/events/geteventlist/MAP"
+# data = requests.get(url)
 
-res = requests.get(url)
+data = requests.get(url).json()
 
 with open('data/gdacs.json', 'w', encoding='utf-8') as f:
-    json.dump(res, f, indent=2)
+    json.dump(data, f, ensure_ascii=False, indent=2)
