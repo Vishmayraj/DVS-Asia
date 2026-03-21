ffb2641d53def50330060dc05a09968e740dbf4d
Vishmayraj <zalavishmayraj@gmail.com>
2026-03-21 12:45:07 +0530

    feat(db): add a base file for connection pooling

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,18 @@
- 
+ # Shared connection pool —> imported by all routers
+
+import psycopg2.pool
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+)
